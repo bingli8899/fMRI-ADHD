71f5933e3623e499d4f18ceb687d6e9318e9ced3
bingli8899 <98918768+bingli8899@users.noreply.github.com>
2025-02-17 20:41:41 -0600

    results for checking missing values

--- scripts/utilities.py
+++
@@ -1,7 +0,0 @@
-# Scripts to save utility function 
-
-def missing_percentage(df, col): 
-    missing = df[col].isnull().sum()
-    missing_percentage = (missing / len(df[col])) * 100 
-    if missing_percentage > 0: 
-        print(f"col {col} has {missing_percentage} % missing.")
