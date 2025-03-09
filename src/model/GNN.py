00ca0c2f3809ce7a5a0aae5d091f3788ef97628f
Bing Li <bli283@wisc.edu>
2025-03-08 19:59:01 -0600

    GNN initialization

--- src/model/GNN.py
+++ src/model/GNN.py
@@ -10,17 +10,19 @@ from dgl.data.utils import makedirs, save_info #, load_info
 
 rootfolder = "/u/b/i/bingl/private/fMRI-AHDH" 
 sys.path.append(os.path.join(rootfolder))
+datafolder = os.path.join(rootfolder, "data")
 from src.data.data_loader import load_or_cache_data 
 
-# Load data:  
-
-# # train_quant = train_data_dic["train_quant"]
-# train_outcome = train_data_dic["train_outcome"]
-# # train_cate = train_data_dic["train_cate"]
-# train_fmri = train_data_dic["train_fmri"] 
-# # test_quant = test_data_dic["test_quant"]
-# # test_cate = test_data_dic["test_cate"]
-# test_fmri = test_data_dic["test_fmri"]
+# Load full data:  
+pickle_file = os.path.join(datafolder, "data.pkl") 
+train_data_dic, test_data_dic = load_or_cache_data(datafolder, pickle_file)
+# train_quant = train_data_dic["train_quant"]
+train_outcome = train_data_dic["train_outcome"]
+# train_cate = train_data_dic["train_cate"]
+train_fmri = train_data_dic["train_fmri"] 
+# test_quant = test_data_dic["test_quant"]
+# test_cate = test_data_dic["test_cate"]
+test_fmri = test_data_dic["test_fmri"]
 
 
 class GraphDataset(DGLDataset): 
