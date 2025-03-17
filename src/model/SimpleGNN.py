b186c2498298306440685e75911895e77fa9fb3e
Bing Li <bli283@wisc.edu>
2025-03-17 16:04:00 -0500

    add simple GNN

--- src/model/SimpleGNN.py
+++ src/model/SimpleGNN.py
@@ -1,43 +1,30 @@
 # Let's use a single GNN without DGL to see the performance first 
 # conda activate simpleGNN 
 
-# This is a simple GNN: 
-# 1) Edge attribute is uni-directional 
-# 2) Only positive values in the matrix is considered as edge_attr, negative values (deactivation) is not considered. 
-# 3) Use identity matrix as node feature 
-# 4) Three layers of covolution. I didn't even found any improvement of using three layers compared to two layers. 
-# 5) I didn't even include the metadata yet. 
-
-# Notes: 
-
-# With global_mean_pool and without dropping out: 
-# 1) With two convolution layers, accuracy after 100 epochs = 76% to 73% (without data dropout)
-# 2) With three convolution layers, accuracy after 100 epochs = 100% --> Is this even real? This doesn't make sense to me 
-# This suggests overfitting probably --> Then, add dropout (dropout rate = 0.5)
-
-# With global_mean_pool and dropping_out rate at 0.5: 
-# With three covolution layers, accuract after 100 epochs = 85% but completely failed on the test dataset :-( 
-
-# Improve this: 
-# 1) Sign of activation should be included as direction 
-# 2) Node feature is one-hot vector instead of identity matrix so the network could learn some unique feature for each node 
-# 3) Fine-tune patameters? 
-# 4) Check if there is an balanced labeling 
-#   --> if not balanced, then I can repeat some lower frequency labeling (Oversampling). 
+""" Improve this: 
+1) Try different pooling 
+2) Normalizaton 
+3) jk (GCN)
+4) 
+"""
 
 
 import sys
 import os 
+import io
 import numpy as np
 import torch as th 
-
+import wandb
+from sklearn.model_selection import KFold
 from torch_geometric.nn import GCNConv, GCN, global_mean_pool, global_add_pool, global_sort_pool, global_max_pool
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader 
 from torch.nn import Dropout, Linear
 from tqdm import tqdm
 import random 
-from imblearn.over_sampling import SMOTE, ADASYN
+from imblearn.over_sampling import SMOTE #, ADASYN
+from torch.optim.lr_scheduler import ReduceLROnPlateau 
+
 
 # Data and function loading: 
 rootfolder = "/u/b/i/bingl/private/fMRI-AHDH/" # change this
@@ -62,6 +49,22 @@ check_label_balance(train_outcome)
 # clearly not balanced: 
 # {'percentage_2': 0.48, 'percentage_0': 0.21, 'percentage_3': 0.18, 'percentage_1': 0.14} 
 
+def make_lovely_matrix(fmri_row): 
+    m = np.zeros((200,200))  
+    for col_name, val in fmri_row.items(): 
+        if col_name == "participant_id": 
+            continue
+
+        node1, node2_temp = col_name.split("throw_")
+        node2 = node2_temp.split("thcolum")[0]
+        # print(node1, node2) 
+
+        if val > 0: 
+            m[int(node1)][int(node2)] = val 
+        elif val < 0: 
+            m[int(node2)][int(node1)] = abs(val) 
+
+    return m 
 
 def create_graph_lst(train_outcome, train_fmri): 
 
@@ -79,22 +82,26 @@ def create_graph_lst(train_outcome, train_fmri):
 
     smote = SMOTE(sampling_strategy="auto", random_state=master_seed)
     fmri_balanced, label_balanced = smote.fit_resample(train_fmri_sorted_connect, train_label["Label"])
-
     train_label_tensor = th.tensor(label_balanced.to_numpy(dtype=np.int16), dtype=th.long)
-    cont_matrix = th.tensor(fmri_balanced.values).float()
 
-    graph_num, node_num = cont_matrix.shape[0], 200 # node_num hard-coded here 
+    graph_num, node_num =  train_label_tensor.shape[0], 200 # node_num hard-coded here 
 
     for i in range(graph_num): 
-        matrix = cont_matrix[i].view(100, 199) # ith row in connectivity matrix 
-        #edge_inx = 
-        edge_inx = (matrix > 0).nonzero(as_tuple = False).t() # uni-directional for now 
-        edge_attr = matrix[edge_inx[0], edge_inx[1]] 
+
+        matrix = make_lovely_matrix(fmri_balanced.iloc[i,:])
+
+        # Direction --> sign on the matrix 
+        edge_inx = matrix.nonzero()
+        edge_inx = [[edge_inx[0][i], edge_inx[1][i]] for i in range(len(edge_inx[0]))]
+        #print(edge_inx.shape)
+        #print("edge_index: ", edge_inx)
+        edge_attr = th.FloatTensor([matrix[idx[0], idx[1]] for idx in edge_inx])
+        #print(edge_attr.shape, edge_inx.shape)
         x = th.eye(node_num) 
-        #x = th.rand((200, 200))
+        #x = th.rand((200, 200) 
 
         graph_data = Data(x = x, # 200 x 200 identity matrix for all node features 
-                          edge_index = edge_inx, 
+                          edge_index = th.LongTensor(edge_inx).transpose(1,0), 
                           edge_attr = edge_attr.clone().detach(), 
                           y = train_label_tensor[i])
         
@@ -171,7 +178,7 @@ class GNN(th.nn.Module):
 
         return x
     
-def train(): 
+def train(train_loader, model, criterion, LinearProjector, optimizer): 
 
     model.train() 
     total_loss, total_samples, correct = 0, 0, 0
@@ -186,7 +193,7 @@ def train():
         # GCN: 
         out = model(data.x, data.edge_index, data.edge_attr, batch_size = 8)
         out = global_mean_pool(out, data.batch)
-        out = LinearProjector1(out)
+        out = LinearProjector(out)
         # out = LinearProjector2(out)
 
         # print(out) # debugging 
@@ -214,9 +221,9 @@ def train():
         predictions = out.argmax(dim=1) 
 
         # de-bugging: 
-        print(f"During training, model output {out}")
-        print(f"During, training, Labels: {data.y}")
-        print(f"During training, prediction: {predictions}")
+        # print(f"Training, model output {out}")
+        # print(f"Training, Labels: {data.y}")
+        # print(f"Training, prediction: {predictions}")
 
         correct += (predictions == data.y).sum().item()
         total_samples += data.y.size(0)
@@ -225,19 +232,20 @@ def train():
 
     return total_loss / len(train_loader), train_accuracy 
 
-def test(): 
+def validate(val_loader, model, criterion, LinearProjector): 
+
     model.eval()
     total_loss, correct, total_samples = 0, 0, 0 
 
     with th.no_grad():  
-        for data in test_loader:
+        for data in val_loader:
 
             # out = model(data) # --> GNN
 
             # GCN: 
             out = model(data.x, data.edge_index, data.edge_attr, batch_size = 8) 
             out = global_mean_pool(out, data.batch)
-            out = LinearProjector1(out)
+            out = LinearProjector(out)
             # out = LinearProjector2(out)
 
             # de-bugging: 
@@ -250,68 +258,168 @@ def test():
             predictions = out.argmax(dim=1)
 
             # de-bugging: 
-            print(f"During testing, model output {out}")
-            print(f"During testing, labels: {data.y}")
-            print(f"During testing, prediction: {predictions}")
+            # print(f"Validating, model output {out}")
+            # print(f"Validating, labels: {data.y}")
+            # print(f"Validating, prediction: {predictions}")
             # print(f"Predictions Shape: {predictions.shape}, Labels Shape: {data.y.shape}")
 
             correct += (predictions == data.y).sum().item()
             
             total_samples += data.y.size(0)
 
-    test_loss = total_loss / len(test_loader)
+    test_loss = total_loss / len(val_loader)
     test_accuracy = (correct / total_samples) * 100
-
-    #model.train() # re-enable dropout 
 
     return test_loss, test_accuracy
 
 
 graph_lst = create_graph_lst(train_outcome, train_fmri) 
-train_data, test_data = data_splitting(graph_lst) # splitting threshold = 0.8 
-
-# GCN: 
-model = GCN(in_channels = 200, 
-            hidden_channels = 200, 
-            num_layers = 4, 
-            out_channels = 200) 
-
+# train_data, test_data = data_splitting(graph_lst) # splitting threshold = 0.8 
+ 
 # GNN: 
 # model = GNN(dropout_rate = 0.1)
 
-LinearProjector1 = Linear(200, 4)
+# Two linear projector: 
+# LinearProjector1 = Linear(200, 128)
 # LinearProjector2 = Linear(128, 4)
 
+# train_loader = DataLoader(train_data, batch_size = 8, shuffle = True)
+# test_loader = DataLoader(test_data, batch_size=8, shuffle=False) 
 
-# Have a learning rate scheduler 
-# Use valudation loss 
-# val_loss = validate() 
-# print the learning rate 
-# Torch.ReduceLROnPlateau 
+def cross_validation(graph_lst, k, initial_lr, num_layers, dropout, batch_size, epochs_num, master_seed, patience): 
 
-optimizer = th.optim.Adam(model.parameters(), lr = 0.0001)
+    kfold = KFold(n_splits = k, shuffle=True, random_state= master_seed)
+    log_messages = [] 
 
-criterion = th.nn.CrossEntropyLoss() # multi-class 
-train_loader = DataLoader(train_data, batch_size = 8, shuffle = True)
-test_loader = DataLoader(test_data, batch_size=8, shuffle=False) 
+    model_choice = "GCN" # change this later. 
+    num_linear_predictors = 1 
+    para_message = (f"""
+        Model: {model_choice},
+        Number of Layers: {num_layers},
+        Number of Linear Predictors: {num_linear_predictors},
+        Initial Learning Rate: {initial_lr},
+        Dropout rate: {dropout},
+        Cross validation fold {k} each with {epochs_num} epochs
+        Batch size {batch_size}, 
+        Master seed: {master_seed},
+        Stopping patience: {patience}
+        """
+        )
+    log_messages.append(para_message)
 
-log_messages = [] 
+    for fold, (train_inx, val_inx) in enumerate(kfold.split(graph_lst)): 
+        
+        lowest_val_loss = float("inf")
+        lowest_valloss_epoch = None 
+        patience_count = 0 
 
-for epoch in range(400+1):
-    train_loss, train_accuracy = train()
-    test_loss, test_accuracy = test() 
-    if epoch % 2 == 0: 
+        # Just use one fold first 
+        # if fold > 0: 
+        #     print("Let's just break the loop at fold 1")
+        #     break 
 
-        message1 = f"Epoch: {epoch}, Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}%"
-        message2 = f"Epoch: {epoch}, Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%" 
+        run = wandb.init(
+        entity="bli283-university-of-wisconsin-madison",
+        project="fmri-adhd",
+        config={
+            "initial_learning_rate": initial_lr,
+            "architecture": "GCN",
+            "dataset": "fmri-only",
+            "num_layers": num_layers,  
+            "epochs": epochs_num,
+            "k-fold": k, 
+            "dropout": dropout, 
+            "batch_size": batch_size, 
+            "master_seed": master_seed, 
+            "patience_to_stop": patience, 
+        },) 
 
-        print(message1)
-        print(message2)
-        log_messages.append(message1)
-        log_messages.append(message2)
 
-with open("training_log_400epochs_TwoLinear.txt", "w") as f:
-    f.write("\n".join(log_messages) + "\n") 
+        train_data = [graph_lst[i] for i in train_inx]
+        val_data = [graph_lst[i] for i in val_inx]
+        train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+        val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
+        best_model_buffer = io.BytesIO() 
+
+        model = GCN(in_channels = 200, 
+            hidden_channels = 200, 
+            num_layers = num_layers, 
+            out_channels = 200, 
+            norm='batch_norm', 
+            act_first=False, # activation after normalization
+            dropout = dropout
+            ) 
+
+        optimizer = th.optim.Adam(model.parameters(), lr = initial_lr)
+        scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True)
+        criterion = th.nn.CrossEntropyLoss() # multi-class
+        LinearProjector = Linear(200, 4) 
+
+        message0 = f"Start training and validating for fold = {fold}"
+        log_messages.append(message0)
+
+        for epoch in range(epochs_num):
+            train_loss, train_accuracy = train(train_loader, model, criterion, LinearProjector, optimizer)
+            val_loss, val_accuracy = validate(val_loader, model, criterion, LinearProjector) 
+            
+            scheduler.step(val_loss) 
+            updated_lr = optimizer.param_groups[0]['lr'] 
+
+            message1 = f"Epoch: {epoch}, Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}%, Updated Learning Rate: {updated_lr}"
+            message2 = f"Epoch: {epoch}, Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy:.2f}%" 
+            print(message1)
+            print(message2)
+            log_messages.append(message1)
+            log_messages.append(message2)
+
+            run.log({"train_acc": train_accuracy,
+                    "train_loss": train_loss,
+                    "val_acc": val_accuracy, 
+                    "val_loss": val_loss, 
+                    "current_learning_rate": updated_lr})
+
+            if val_loss <= lowest_val_loss: 
+                lowest_val_loss = val_loss
+                lowest_valloss_epoch = epoch 
+                patience_count = 0 
+
+                best_model_buffer.seek(0)  
+                th.save(model.state_dict(), best_model_buffer)
+            else: 
+                patience_count += 1
+            
+            if patience_count > patience: 
+                log_messages.append(f"Validation loss reached a plateau at epoch {lowest_valloss_epoch}, break")
+                break
+
+        run.finish()
+    
+    best_model_buffer.seek(0)
+    with open(f"GCN_layer{num_layers}.pth", "wb") as f:
+        f.write(best_model_buffer.read())  
+
+    return log_messages 
+
+def main(): 
+    
+    k = 5
+    initial_lr = 0.001
+    num_layers = 3
+    dropout = 0.1
+    batch_size = 8
+    epochs_num = 250
+    master_seed = 54321
+    patience = 20
+
+    logfile = f"GCN_fold{k}_iniLr{initial_lr}_layers{num_layers}_dropout{dropout}_batch{batch_size}_epochs{epochs_num}_seed{master_seed}_patience{patience}_batchnormalization.log"
+
+    log_messages = cross_validation(graph_lst, k, initial_lr, num_layers, dropout, batch_size, epochs_num, master_seed, patience)
+
+    with open(logfile, "w") as f:
+        f.write("\n".join(log_messages) + "\n")  
+
+if __name__ == "__main__":
+    main() 
 
 
 
